49144c315c84471a5385967e225c72ab5dcd78b9
Isaac Hughes <UnoVex11223@gmail.com>
2025-10-05 14:08:38 -0400

    Update List_to_Dictionary_Loot_Conversion.py

--- Chapter_7/List_to_Dictionary_Loot_Conversion.py
+++ Chapter_7/List_to_Dictionary_Loot_Conversion.py
@@ -1,1 +1,24 @@
+from Fantasy_Game_Inventory import display_inventory
 
+def add_to_inventory(inventory: dict, added_items: list):
+    """
+    Adds items from a list to an inventory dictionary.
+
+    Args:
+        inventory: The dictionary to update.
+        added_items: A list of strings representing the items to add.
+
+    Returns:
+        The inventory dictionary updated with the new items.
+    """
+    for item in added_items:
+        inventory.setdefault(item, 0)
+        inventory[item] += 1
+    return inventory
+
+
+inv = {'gold coin': 42, 'rope': 1}
+dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+inv = add_to_inventory(inv, dragon_loot)
+display_inventory(inv)
